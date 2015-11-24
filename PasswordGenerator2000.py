76c9039eae5b07d6cde531899d2df724a5b5e7d8
Steve Clement <steve@localhost.lu>
2015-11-24 15:13:02 +0100

    -  replaced tabs with 2-spaces

--- PasswordGenerator2000.py
+++ PasswordGenerator2000.py
@@ -24,34 +24,34 @@ print("You are running " + inspect.getfile(inspect.currentframe()) + " version: 
 Passwordlength = input ("How long should your password be?\n")
 
 if Passwordlength.isdigit():
-	Choice1 = input ("Are you sure that your password should be " + str(Passwordlength) + " long\n")
-	if Choice1 != "yes" and Choice1 != "Yes":
-		os._exit(0)
+  Choice1 = input ("Are you sure that your password should be " + str(Passwordlength) + " long\n")
+  if Choice1 != "yes" and Choice1 != "Yes":
+    os._exit(0)
 
-	Choice2 = input ("Are you really sure?\n")
-	if Choice2 != "yes" and Choice2 != "Yes" :
-		os._exit(0)
+  Choice2 = input ("Are you really sure?\n")
+  if Choice2 != "yes" and Choice2 != "Yes" :
+    os._exit(0)
 
-	Choice3 = input ("So we continue?\n")
-	if Choice3 != "yes" and Choice3 != "Yes" :
-		os._exit(0)
+  Choice3 = input ("So we continue?\n")
+  if Choice3 != "yes" and Choice3 != "Yes" :
+    os._exit(0)
 
-	Choice4 = input ("Just be sure, type 'Yes' one more time\n")
-	if Choice4 != "yes" and Choice4 != "Yes" :
-			os._exit(0)
+  Choice4 = input ("Just be sure, type 'Yes' one more time\n")
+  if Choice4 != "yes" and Choice4 != "Yes" :
+      os._exit(0)
 
-	Choice5 = input ("Okay last time. Type 'Yes' to continue\n")
-	if Choice5 != "yes" and Choice5 != "Yes" :
-		os._exot(0)
+  Choice5 = input ("Okay last time. Type 'Yes' to continue\n")
+  if Choice5 != "yes" and Choice5 != "Yes" :
+    os._exot(0)
 
-	Choice6 = input ("Are you aware that you won't be able to go back after this?\n")
-	if Choice6 != "yes" and Choice6 != "Yes" :
-		os._exit(0)
+  Choice6 = input ("Are you aware that you won't be able to go back after this?\n")
+  if Choice6 != "yes" and Choice6 != "Yes" :
+    os._exit(0)
 
-	finishedPassword = ""
+  finishedPassword = ""
 
-	for x in range(int(Passwordlength)):
-		randomNumber = random.randint(0,total)
-		finishedPassword += (AbcBig + AbcSmall + numbers)[randomNumber]
+  for x in range(int(Passwordlength)):
+    randomNumber = random.randint(0,total)
+    finishedPassword += (AbcBig + AbcSmall + numbers)[randomNumber]
 
-	print(finishedPassword)
+  print(finishedPassword)
