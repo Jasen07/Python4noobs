443fc815a9764b60750bc22989b00f28b43a7924
Gil Rippinger <rippingergil@hotmail.com>
2015-11-24 14:07:34 +0100

    first commit

---
+++ PasswordGenerator2000.py
@@ -0,0 +1,62 @@
+
+#PasswordGenerator2000 3.3.1
+
+#randomly generated password
+#6 to 12 characters long
+#only AbcBig AbcSmall and numbers
+#no symbols
+#input : password length, are you sure (till sucked)
+#output : "to get a generated password please buy the advanced pack for 9.99$"
+
+import os, random
+
+# List of allowed characters in the password
+AbcSmall = "abcdefghijklmnopqrstuvxyz"
+AbcBig = "ABCDEFGHIJKLMNOPQRSTUVXYZ"
+numbers = "0123456789"
+total = len(AbcSmall) + len(AbcBig) + len(numbers) - 1
+
+
+Passwordlength = input ("How long should your password be?\n")
+
+if Passwordlength.isdigit():
+	
+	Choice1 = input ("Are you sure that your password should be " + str(Passwordlength) + " long\n")
+	if Choice1 != "yes" and Choice1 != "Yes":
+		os._exit(0)
+
+	Choice2 = input ("Are you really sure?\n")
+	if Choice2 != "yes" and Choice2 != "Yes" :
+		os._exit(0)
+
+	Choice3 = input ("So we continue?\n")
+	if Choice3 != "yes" and Choice3 != "Yes" :
+		os._exit(0)
+		
+	Choice4 = input ("Just be sure, type 'Yes' one more time\n")
+	if Choice4 != "yes" and Choice4 != "Yes" :
+			os._exit(0)
+	
+	Choice5 = input ("Okay last time. Type 'Yes' to continue\n")
+	if Choice5 != "yes" and Choice5 != "Yes" :
+		os._exot(0)
+			
+	Choice6 = input ("Are you aware that you won't be able to go back after this?\n")
+	if Choice6 != "yes" and Choice6 != "Yes" :
+		os._exit(0)
+	
+	
+	finishedPassword = ""
+
+	for x in range(int(Passwordlength)):
+		randomNumber = random.randint(0,total)
+		finishedPassword += (AbcBig + AbcSmall + numbers)[randomNumber]
+
+	print(finishedPassword)
+
+
+
+
+
+
+
